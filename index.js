92dfb1588bb2473353652ddf0f25aa5e9ae01e6f
suprabhatkumar <suprabhatniraj@gmail.com>
2023-09-26 18:30:19 +0530

    put,get,delete,find api do tttest

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 require('./config')
 const Product=require('./product');
+const product = require('./product');
 
 const app=express();
 
@@ -8,8 +9,25 @@ app.use(express.json()); //for converting string data
 app.post("/",async (req,res)=>{
     let data = new Product(req.body);
     let result= await data.save();
-    console.log(req.body);
+    console.log(req.body); 
     console.log(result);
     res.send(result)
 })
+app.get("/list",async (req,res)=>{
+    let data= await Product.find();
+    res.send(data);
+})
+app.delete("/delete/:_id", async (req, res) => {
+    console.log(req.params); 
+    let data = await Product.deleteOne(req.params); 
+    res.send(data);
+})
+app.put("/update/:_id", async (req, res) => {
+    // console.log(req.params);
+    let data = await Product.updateOne(
+        req.params,
+        {$set:req.body}
+    );
+    res.send(data);
+});
 app.listen(5000);
